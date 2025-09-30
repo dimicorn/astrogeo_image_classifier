2b5caa171af90194720c2ee1c4f0f082a02388b8
Dmitrii <zagorulia.ds@phystech.edu>
2025-09-30 15:33:26 +0300

    previous committed changes

--- fill_db_filtered_map.py
+++ fill_db_filtered_map.py
@@ -1,15 +1,20 @@
 from warnings import filterwarnings
+import argparse
 from yaml import safe_load
 from munch import munchify
 from utils.fill_table import FillTable
 
 
 def main() -> None:
+	parser = argparse.ArgumentParser()
+	parser.add_argument('-map', '--map_table', type=str, help='SQL table which will be created and filled')
+	args = parser.parse_args()
 	filterwarnings('ignore')
+
 	with open('config.yaml') as f:
 		cfg = munchify(safe_load(f))
 	ft = FillTable(cfg, 'map')
-	ft.fill('maps_Apr25_test')
+	ft.fill(args.map_table)
 
 if __name__ == '__main__':
 	main()
--- fill_db_filtered_uv.py
+++ fill_db_filtered_uv.py
@@ -1,15 +1,21 @@
 from warnings import filterwarnings
+import argparse
 from yaml import safe_load
 from munch import munchify
 from utils.fill_table import FillTable
 
 
 def main() -> None:
+	parser = argparse.ArgumentParser()
+	parser.add_argument('-uv', '--uv_table', type=str, help='SQL table which will be created and filled')
+	parser.add_argument('-map', '--map_table', type=str, help='Existing SQL table which will be used for UV data quality')
+	args = parser.parse_args()
 	filterwarnings('ignore')
+
 	with open('config.yaml') as f:
 		cfg = munchify(safe_load(f))
 	ft = FillTable(cfg, 'uv')
-	ft.fill('uvs_Apr25_test', 'maps_Apr25_test')
+	ft.fill(args.uv_table, args.map_table)
 
 if __name__ == '__main__':
 	main()
